5f855994e0f2b4590de82b8f65149298ce744474
calin <calinoiu.alexandru@agilefreaks.com>
2021-03-04 10:10:19 +0000

    Add linux dev container

--- Core/StuckInTheMudRoll.cs
+++ Core/StuckInTheMudRoll.cs
@@ -1,6 +1,4 @@
-﻿using System;
-
-namespace Core
+﻿namespace Core
 {
     public class StuckInTheMudRoll
     {
@@ -12,11 +10,11 @@ namespace Core
 
         public StuckInTheMudRoll(int d1, int d2, int d3, int d4, int d5)
         {
-            this._d1  = d1;
-            this._d2  = d2;
-            this._d3  = d3;
-            this._d4  = d4;
-            this._d5  = d5;
+            this._d1 = d1;
+            this._d2 = d2;
+            this._d3 = d3;
+            this._d4 = d4;
+            this._d5 = d5;
         }
 
         public int Score()
@@ -42,12 +40,12 @@ namespace Core
 
         public static int dice_available_next_roll(int d1, int d2, int d3, int d4, int d5)
         {
-            var dice = new [] { d1, d2, d3, d4, d5 };
+            var dice = new[] { d1, d2, d3, d4, d5 };
             int total_dice = 5;
-            for (var i = 0; i < dice.Length; i ++)
+            for (var i = 0; i < dice.Length; i++)
             {
                 int d = dice[i];
-                if (d == 2 || d == 5) 
+                if (d == 2 || d == 5)
                 {
                     total_dice -= 1;
                 }
